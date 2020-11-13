bed481942a524bb5fd8aa8b50ef8246022c01857
Georg Meißner <supergeorg@gmail.com>
2020-11-13 12:01:14 +0100

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Georg Meissner on 13.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/Grocy_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grocy_SwiftUIApp.swift
+//  Shared
+//
+//  Created by Georg Meissner on 13.11.20.
+//
+
+import SwiftUI
+
+@main
+struct Grocy_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
