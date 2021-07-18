c98ea821ba75cf8d2a0336b228b28b0efd697c8b
Georg Meißner <supergeorg@gmail.com>
2021-07-18 14:26:47 +0200

    Added transaction type self-production.

--- Shared/Model/Stock/Transaction/TransactionType.swift
+++ Shared/Model/Stock/Transaction/TransactionType.swift
@@ -9,12 +9,13 @@ import Foundation
 import SwiftUI
 
 enum TransactionType: String, Codable, CaseIterable {
-    case purchase = "purchase"
     case consume = "consume"
     case inventoryCorrection = "inventory-correction"
     case productOpened = "product-opened"
-    case stockEditOld = "stock-edit-old"
+    case purchase = "purchase"
+    case selfProduction = "self-production"
     case stockEditNew = "stock-edit-new"
+    case stockEditOld = "stock-edit-old"
     case transferFrom = "transfer_from"
     case transferTo = "transfer_to"
     
@@ -22,16 +23,18 @@ enum TransactionType: String, Codable, CaseIterable {
         switch self {
         case .consume:
             return LocalizedStringKey("tr.consume")
-        case .purchase:
-            return LocalizedStringKey("tr.purchase")
         case .inventoryCorrection:
             return LocalizedStringKey("tr.inventoryCorrection")
         case .productOpened:
             return LocalizedStringKey("tr.opened")
-        case .stockEditOld:
-            return LocalizedStringKey("tr.stockEditOld")
+        case .purchase:
+            return LocalizedStringKey("tr.purchase")
+        case .selfProduction:
+            return LocalizedStringKey("tr.selfProduction")
         case .stockEditNew:
             return LocalizedStringKey("tr.stockEditNew")
+        case .stockEditOld:
+            return LocalizedStringKey("tr.stockEditOld")
         case .transferFrom:
             return LocalizedStringKey("tr.transferFrom")
         case .transferTo:
