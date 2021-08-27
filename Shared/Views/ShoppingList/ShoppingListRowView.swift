5c50d25099928182c08027a3c2ee0e5a5b28d3a4
Georg Meißner <supergeorg@gmail.com>
2021-08-27 17:26:52 +0200

    Small cleanup and improvement to values in shopping list.

--- Shared/Views/ShoppingList/ShoppingListRowView.swift
+++ Shared/Views/ShoppingList/ShoppingListRowView.swift
@@ -19,19 +19,17 @@ struct ShoppingListRowView: View {
     
     var product: MDProduct? {
         grocyVM.mdProducts.first(where: {$0.id == shoppingListItem.productID})
-//            ?? MDProduct(id: 0, name: "Error Product", mdProductDescription: "error", productGroupID: 0, active: 0, locationID: 0, shoppingLocationID: nil, quIDPurchase: 0, quIDStock: 0, quFactorPurchaseToStock: 0, minStockAmount: 0, defaultBestBeforeDays: 0, defaultBestBeforeDaysAfterOpen: 0, defaultBestBeforeDaysAfterFreezing: "0", defaultBestBeforeDaysAfterThawing: "0", pictureFileName: nil, enableTareWeightHandling: "0", tareWeight: "0", notCheckStockFulfillmentForRecipes: "0", parentProductID: nil, calories: "0", cumulateMinStockAmountOfSubProducts: "0", dueType: "0", quickConsumeAmount: "0", hideOnStockOverview: 0, rowCreatedTimestamp: "0", userfields: nil)
     }
     
     var quantityUnit: MDQuantityUnit? {
         grocyVM.mdQuantityUnits.first(where: {$0.id == product?.quIDStock})
-//            ?? MDQuantityUnit(id: 0, name: "Error QU", mdQuantityUnitDescription: nil, rowCreatedTimestamp: "", namePlural: "Error QU", pluralForms: nil, userfields: nil)
     }
     
     var amountString: String {
         if let quantityUnit = quantityUnit {
-            return "\(shoppingListItem.amount) \(shoppingListItem.amount == 1 ? quantityUnit.name : quantityUnit.namePlural)"
+            return "\(formatAmount(shoppingListItem.amount)) \(shoppingListItem.amount == 1 ? quantityUnit.name : quantityUnit.namePlural)"
         } else {
-            return "\(shoppingListItem.amount) ?"
+            return "\(formatAmount(shoppingListItem.amount)) ?"
         }
     }
     
