a3f80f9e785a00d7219b04168d77918e00957a2f
Georg Meißner <supergeorg@gmail.com>
2021-11-12 19:39:01 +0100

    Added URL, OS and client info to the server info view.

--- Shared/Views/Settings/GrocyInfoView.swift
+++ Shared/Views/Settings/GrocyInfoView.swift
@@ -10,12 +10,28 @@ import SwiftUI
 struct GrocyInfoView: View {
     var systemInfo: SystemInfo
     
+    @AppStorage("localizationKey") var localizationKey: String = "en"
+    @AppStorage("isDemoModus") var isDemoModus: Bool = false
+    @AppStorage("grocyServerURL") var grocyServerURL: String = ""
+    @AppStorage("demoServerURL") var demoServerURL: String = GrocyAPP.DemoServers.noLanguage.rawValue
+    
     var body: some View {
-        Form(){
+        List{
+            if isDemoModus {
+                Text(demoServerURL)
+            } else {
+                Text(grocyServerURL)
+            }
             Text(LocalizedStringKey("str.settings.info.grocyVersion \(systemInfo.grocyVersion.version)"))
-            Text(LocalizedStringKey("str.settings.info.grocyRLSDate \(formatDateOutput(systemInfo.grocyVersion.releaseDate) ?? formatTimestampOutput(systemInfo.grocyVersion.releaseDate))"))
+            Text(LocalizedStringKey("str.settings.info.grocyRLSDate \(formatDateOutput(systemInfo.grocyVersion.releaseDate) ?? formatTimestampOutput(systemInfo.grocyVersion.releaseDate, localizationKey: localizationKey) ?? "")"))
             Text(LocalizedStringKey("str.settings.info.grocyPHPVersion \(systemInfo.phpVersion)"))
             Text(LocalizedStringKey("str.settings.info.grocySQLiteVersion \(systemInfo.sqliteVersion)"))
+            if let os = systemInfo.os {
+                Text(os)
+            }
+            if let client = systemInfo.client {
+                Text(client)
+            }
         }
         .navigationTitle("Grocy")
     }
@@ -24,7 +40,7 @@ struct GrocyInfoView: View {
 struct GrocyInfoView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView(){
-            GrocyInfoView(systemInfo: SystemInfo(grocyVersion: SystemInfo.GrocyVersion(version: "version", releaseDate: "date"), phpVersion: "php", sqliteVersion: "sqlite"))
+            GrocyInfoView(systemInfo: SystemInfo(grocyVersion: SystemInfo.GrocyVersion(version: "version", releaseDate: "date"), phpVersion: "php", sqliteVersion: "sqlite", os: "iOS", client: "Grocy Mobile"))
         }
     }
 }
