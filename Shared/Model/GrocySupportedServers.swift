bd4e6032dbe0375bba769bb53117b181ecec9499
Georg Meißner <supergeorg@gmail.com>
2022-01-14 19:10:22 +0100

    Added french demo server to selection

--- Shared/Model/GrocySupportedServers.swift
+++ Shared/Model/GrocySupportedServers.swift
@@ -8,13 +8,14 @@
 import Foundation
 
 struct GrocyAPP {
-//    The app plans to support the newest Grocy version as well as the version included in Home Assistant
+    //    The app plans to support the newest Grocy version as well as the version included in Home Assistant
     static let supportedVersions: [String] = ["3.1.3"]
     
     enum DemoServers: String, CaseIterable, Identifiable {
         case noLanguage = "https://demo.grocy.info"
         case english = "https://en.demo.grocy.info"
         case german = "https://de.demo.grocy.info"
+        case french = "https://fr.demo.grocy.info"
         case polish = "https://pl.demo.grocy.info"
         case develop = "https://test-xjixc1minhzshgy6o142.demo.grocy.info"
         
@@ -30,6 +31,8 @@ struct GrocyAPP {
                 return "English Grocy server"
             case .german:
                 return "German Grocy server"
+            case .french:
+                return "French Grocy server"
             case .polish:
                 return "Polish Grocy server"
             case .develop:
