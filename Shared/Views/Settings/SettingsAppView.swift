ad22d57f3e6c0bc4aba7c30f6b344d79ad6f7f5c
Georg Meißner <supergeorg@gmail.com>
2022-11-18 18:18:55 +0100

    Added a setting for auto refreshing data
    The corresponding functionality is missing yet.

--- Shared/Views/Settings/SettingsAppView.swift
+++ Shared/Views/Settings/SettingsAppView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct SettingsAppView: View {
     @AppStorage("quickScanActionAfterAdd") private var quickScanActionAfterAdd: Bool = false
+    @AppStorage("autoReload") private var autoReload: Bool = false
+    @AppStorage("autoReloadInterval") private var autoReloadInterval: Int = 0
+    
+    let refreshIntervals: [Int] = [3, 5, 10, 30, 60]
     
     var body: some View {
         Form {
@@ -17,6 +21,22 @@ struct SettingsAppView: View {
                 MyToggle(isOn: $quickScanActionAfterAdd, description: "str.settings.app.quickScan.actionAfterAdd")
             }
 #endif
+            Section(header: Text(LocalizedStringKey("str.settings.app.update")).font(.title)) {
+                MyToggle(isOn: $autoReload, description: "str.settings.app.update.autoReload")
+                Picker(selection: $autoReloadInterval, content: {
+                    Text("").tag(0)
+                    ForEach(refreshIntervals, id:\.self, content: { interval in
+                        Text("\(interval.formatted())s").tag(interval)
+                    })
+                }, label: {
+                    Label(title: {
+                        Text(LocalizedStringKey("str.settings.app.update.autoReload.interval"))
+                    }, icon: {
+                        Image(systemName: MySymbols.timedRefresh)
+                    })
+                }
+                )
+            }
         }
         .navigationTitle(LocalizedStringKey("str.settings.app"))
     }
