3d0ad7cb8da763133769965b57b26dd508cab059
Georg Meißner <supergeorg@gmail.com>
2022-12-20 20:01:38 +0100

    Update shopping list settings
    Added icons, but sync is still not ready yet.

--- Shared/Views/Settings/SettingsShoppingListView.swift
+++ Shared/Views/Settings/SettingsShoppingListView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct SettingsShoppingListView: View {
     @StateObject var grocyVM: GrocyViewModel = .shared
     
+    @AppStorage("devMode") private var devMode: Bool = false
+    @AppStorage("syncShoppingListToReminders") private var syncShoppingListToReminders: Bool = false
+    @AppStorage("shoppingListToSyncID") private var shoppingListToSyncID: Int = 0
+    
     @State private var useAutoAddBelowMinStockAmount: Bool = false
     
     @State private var isFirst: Bool = true
@@ -30,12 +34,76 @@ struct SettingsShoppingListView: View {
     var content: some View {
         Form {
             Section(header: Text(LocalizedStringKey("str.settings.shoppingList.shoppingList")).font(.title)) {
-                ServerSettingsToggle(settingKey: GrocyUserSettings.CodingKeys.shoppingListAutoAddBelowMinStockAmount.rawValue, description: "str.settings.shoppingList.autoAddToShL", toggleFeedback: $useAutoAddBelowMinStockAmount)
-                ServerSettingsObjectPicker(settingKey: GrocyUserSettings.CodingKeys.shoppingListAutoAddBelowMinStockAmountListID.rawValue, description: "str.shL", objects: .shoppingLists)
+                ServerSettingsToggle(
+                    settingKey: GrocyUserSettings.CodingKeys.shoppingListAutoAddBelowMinStockAmount.rawValue,
+                    description: "str.settings.shoppingList.autoAddToShL",
+                    icon: MySymbols.amount,
+                    toggleFeedback: $useAutoAddBelowMinStockAmount
+                )
+                ServerSettingsObjectPicker(
+                    settingKey: GrocyUserSettings.CodingKeys.shoppingListAutoAddBelowMinStockAmountListID.rawValue,
+                    description: "str.shL",
+                    icon: MySymbols.shoppingList,
+                    objects: .shoppingLists
+                )
                     .disabled(!useAutoAddBelowMinStockAmount)
             }
             Section(header: Text(LocalizedStringKey("str.settings.shoppingList.shLToStockWF")).font(.title)) {
-                ServerSettingsToggle(settingKey: GrocyUserSettings.CodingKeys.shoppingListToStockWorkflowAutoSubmitWhenPrefilled.rawValue, description: "str.settings.shoppingList.autoAddToStock")
+                ServerSettingsToggle(
+                    settingKey: GrocyUserSettings.CodingKeys.shoppingListToStockWorkflowAutoSubmitWhenPrefilled.rawValue,
+                    description: "str.settings.shoppingList.autoAddToStock",
+                    icon: MySymbols.stockOverview
+                )
+            }
+            if devMode {
+                Section(header: Text(LocalizedStringKey("REMINDER SYNC")).font(.title)) {
+                    MyToggle(isOn: $syncShoppingListToReminders, description: "SYNC SHOPPING LIST TO REMINDERS")
+                    Picker("SHOPPING LIST", selection: $shoppingListToSyncID, content: {
+                        Text("").tag(0)
+                        ForEach(grocyVM.shoppingListDescriptions, id:\.id) { shoppingListDescription in
+                            Text(shoppingListDescription.name).tag(shoppingListDescription.id)
+                        }
+                    })
+                    .disabled(!syncShoppingListToReminders)
+                    Button(action: {
+                        Task {
+                            do {
+                                try await ReminderStore.shared.requestAccess()
+                                ReminderStore.shared.initCalendar()
+                            } catch {
+                                print(error)
+                            }
+                        }
+                    }, label: {
+                        Text("INIT CALENDAR")
+                    })
+                    Button(action: {
+                        do {
+                            for shoppingListItem in grocyVM.shoppingList {
+                                let title = "\(shoppingListItem.amount.formattedAmount) \(grocyVM.mdProducts.first(where: { $0.id == shoppingListItem.productID })?.name ?? "\(shoppingListItem.productID ?? 0)")"
+                                try ReminderStore.shared.save(Reminder(title: title, isComplete: shoppingListItem.done == 1))
+                            }
+                        } catch {
+                            print(error)
+                        }
+                    }, label: {
+                        Text("ADD TO CALENDAR")
+                    })
+                    .disabled(!ReminderStore.shared.isAvailable_)
+                    Button(action: {
+                        Task {
+                            do {
+                                let allReminders = try await ReminderStore.shared.readAll()
+                                grocyVM.updateShoppingListFromReminders(reminders: allReminders)
+                            } catch {
+                                print(error)
+                            }
+                        }
+                    }, label: {
+                        Text("READ ALL")
+                    })
+                    .disabled(!ReminderStore.shared.isAvailable_)
+                }
             }
         }
         .navigationTitle(LocalizedStringKey("str.settings.shoppingList"))
