c5d245f2600096124f7abfdc9cdecaea764a0903
Georg Meißner <supergeorg@gmail.com>
2023-09-08 17:32:20 +0200

    Add 4.0.3 to list of supported servers

--- Shared/Model/GrocySupportedServers.swift
+++ Shared/Model/GrocySupportedServers.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct GrocyAPP {
     //    The app plans to support the newest Grocy version as well as the version included in Home Assistant
-    static let supportedVersions: [String] = ["4.0.2"]
+    static let supportedVersions: [String] = ["4.0.2", "4.0.3"]
     
     enum DemoServers: String, CaseIterable, Identifiable {
         case noLanguage = "https://demo.grocy.info"
