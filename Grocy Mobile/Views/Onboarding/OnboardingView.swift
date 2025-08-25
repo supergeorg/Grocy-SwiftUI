3156c7967621f3f3e4b40741c0385b40a7819a79
Georg Meißner <supergeorg@gmail.com>
2025-08-25 20:54:42 +0200

    Onboarding view: Redesign
    Use a colorful background and a liquid glass button for start

--- Grocy Mobile/Views/Onboarding/OnboardingView.swift
+++ Grocy Mobile/Views/Onboarding/OnboardingView.swift
@@ -9,17 +9,56 @@ import SwiftUI
 
 struct OnboardingDevicesView: View {
     var body: some View {
-        HStack{
-#if os(macOS)
-            Image(systemName: "macbook")
-            Image(systemName: "macmini")
-            Image(systemName: "desktopcomputer")
-#elseif os(iOS)
-            Image(systemName: "iphone")
-            Image(systemName: "iphone.homebutton")
-            Image(systemName: "ipad")
-            Image(systemName: "ipad.homebutton")
-#endif
+        HStack {
+            #if os(macOS)
+                Image(systemName: "macbook")
+                Image(systemName: "macmini")
+                Image(systemName: "desktopcomputer")
+            #elseif os(iOS)
+                Image(systemName: "iphone")
+                Image(systemName: "iphone.homebutton")
+                Image(systemName: "ipad")
+                Image(systemName: "ipad.homebutton")
+            #endif
+        }
+    }
+}
+
+public enum OnboardingCards: CaseIterable {
+    case GROCY
+    case ERP_BEYOND
+    case IOS_MACOS
+
+    public var title: LocalizedStringKey {
+        switch self {
+        case .GROCY:
+            return "Grocy"
+        case .ERP_BEYOND:
+            return "grocy - ERP beyond your fridge"
+        case .IOS_MACOS:
+            return "Grocy for iOS/macOS"
+        }
+    }
+
+    public var subtitle: LocalizedStringKey {
+        switch self {
+        case .GROCY:
+            return ""
+        case .ERP_BEYOND:
+            return "grocy is a web-based self-hosted groceries & household management solution for your home."
+        case .IOS_MACOS:
+            return "An app in native design, allowing a comfortable use of Grocy at home and on the go."
+        }
+    }
+
+    public var imageName: String {
+        switch self {
+        case .GROCY:
+            return "grocy-logo"
+        case .ERP_BEYOND:
+            return "web-stock"
+        case .IOS_MACOS:
+            return "stock-screenshot"
         }
     }
 }
@@ -27,7 +66,7 @@ struct OnboardingDevicesView: View {
 struct OnboardingCardView: View {
     var card: OnboardingCards
     var body: some View {
-        VStack{
+        VStack {
             Image(card.imageName)
                 .resizable()
                 .scaledToFit()
@@ -47,83 +86,48 @@ struct OnboardingCardView: View {
 
 struct OnboardingView: View {
     @AppStorage("onboardingNeeded") var onboardingNeeded: Bool = true
-    
+
     var body: some View {
-        VStack{
-#if os(iOS)
-            TabView{
-                ForEach(OnboardingCards.allCases, id:\.self) {card in
-                    OnboardingCardView(card: card)
+        VStack {
+            #if os(iOS)
+                TabView {
+                    ForEach(OnboardingCards.allCases, id: \.self) { card in
+                        OnboardingCardView(card: card)
+                    }
                 }
-            }
-            .tabViewStyle(PageTabViewStyle())
-#else
-            HStack{
-                ForEach(OnboardingCards.allCases, id:\.self) {card in
-                    OnboardingCardView(card: card)
+                .tabViewStyle(PageTabViewStyle())
+            #else
+                HStack {
+                    ForEach(OnboardingCards.allCases, id: \.self) { card in
+                        OnboardingCardView(card: card)
+                    }
                 }
-            }
-#endif
-            Button(action: {
-                onboardingNeeded = false
-            }, label: {
-                Text("Let's get started!")
-#if os(iOS)
-//                    .padding(30)
-//                    .background(.blue)
-//                    .foregroundStyle(.white)
-//                    .clipShape(.capsule)
-#endif
-            })
-            .buttonStyle(.borderedProminent)
-            .buttonBorderShape(.capsule)
-            .controlSize(.extraLarge)
+            #endif
+            Button(
+                action: {
+                    onboardingNeeded = false
+                },
+                label: {
+                    Text("Let's get started!")
+                        #if os(iOS)
+                            .font(.title)
+                            .foregroundStyle(.white)
+                            .padding()
+                            .glassEffect(.regular.tint(.GrocyColors.grocyGreen).interactive())
+                        #endif
+                }
+            )
         }
+        .background(Color(.GrocyColors.grocyBlueBackground))
     }
 }
 
-struct OnboardingView_Previews: PreviewProvider {
-    static var previews: some View {
-        OnboardingView()
-    }
+#Preview {
+    OnboardingView()
+        .colorScheme(.light)
 }
 
-public enum OnboardingCards: CaseIterable {
-    case GROCY
-    case ERP_BEYOND
-    case IOS_MACOS
-    
-    public var title: LocalizedStringKey {
-        switch self {
-        case .GROCY:
-            return "Grocy"
-        case .ERP_BEYOND:
-            return "grocy - ERP beyond your fridge"
-        case .IOS_MACOS:
-            return "Grocy for iOS/macOS"
-        }
-    }
-    
-    public var subtitle: LocalizedStringKey {
-        switch self {
-        case .GROCY:
-            return ""
-        case .ERP_BEYOND:
-            return "grocy is a web-based self-hosted groceries & household management solution for your home."
-        case .IOS_MACOS:
-            return "An app in native design, allowing a comfortable use of Grocy at home and on the go."
-        }
-    }
-    
-    public var imageName: String {
-        switch self {
-        case .GROCY:
-            return "grocy-logo"
-        case .ERP_BEYOND:
-            return "web-stock"
-        case .IOS_MACOS:
-            return "stock-screenshot"
-        }
-    }
+#Preview("Dark Mode") {
+    OnboardingView()
+        .colorScheme(.dark)
 }
-
