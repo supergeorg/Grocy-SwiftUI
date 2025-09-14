ee9367c9b93909fb15e0a501c8df7894a04aabe1
Georg Meißner <supergeorg@gmail.com>
2025-09-14 14:21:03 +0200

    Stock Product Info: Use form and labeled content for better UI

--- Grocy Mobile/Views/Stock/StockProductInfoView.swift
+++ Grocy Mobile/Views/Stock/StockProductInfoView.swift
@@ -5,12 +5,12 @@
 //  Created by Georg Meissner on 30.11.20.
 //
 
-import SwiftUI
 import SwiftData
+import SwiftUI
 
 struct StockProductInfoView: View {
     @Environment(GrocyViewModel.self) private var grocyVM
-    
+
     @Query() var detailsList: [StockProductDetails]
     var productDetails: StockProductDetails? {
         return detailsList.first(where: { $0.productID == stockElement.productID })
@@ -19,71 +19,105 @@ struct StockProductInfoView: View {
     var product: MDProduct? {
         productList.first(where: { $0.id == stockElement.productID })
     }
-    
-    @AppStorage("localizationKey") var localizationKey: String = "en"
-    
-    var stockElement: StockElement
-    
-    var body: some View {
-        List {
-            if let productDetails = productDetails {
-                Group {
-                    (Text("Stock amount") + Text(": ")).bold()
-                    +
-                    Text("\(productDetails.stockAmount.formattedAmount) \(productDetails.quantityUnitStock?.getName(amount: productDetails.stockAmount) ?? "")")
-                    
-                    (Text("Stock value") + Text(": ")).bold()
-                    +
-                    Text(grocyVM.getFormattedCurrency(amount: productDetails.stockValue ?? 0.0))
-                    
-                    (Text("Default location") + Text(": ")).bold()
-                    +
-                    Text(productDetails.location?.name ?? "")
-                }
-                
-                Group {
-                    (Text("Last purchased") + Text(": ")).bold()
-                    +
-                    Text(LocalizedStringKey(formatDateAsString(productDetails.lastPurchased, localizationKey: localizationKey) ?? "Never"))
-                    +
-                    Text(" ")
-                    +
-                    Text(getRelativeDateAsText(productDetails.lastPurchased, localizationKey: localizationKey) ?? "")
-                        .font(.caption)
-                        .italic()
-                    
-                    (Text("Last used") + Text(": ")).bold()
-                    +
-                    Text(LocalizedStringKey(formatDateAsString(productDetails.lastUsed, localizationKey: localizationKey) ?? "Never"))
-                    +
-                    Text(" ")
-                    +
-                    Text(getRelativeDateAsText(productDetails.lastUsed, localizationKey: localizationKey) ?? "")
-                        .font(.caption)
-                        .italic()
-                }
-                
-                Group {
-                    (Text("Last price") + Text(": ")).bold()
-                    +
-                    Text(productDetails.lastPrice != nil ? "\(grocyVM.getFormattedCurrency(amount: productDetails.lastPrice ?? 0.0)) per \(productDetails.quantityUnitStock?.name ?? "")" : "Unknown")
-                    
-                    (Text("Average price") + Text(": ")).bold()
-                    +
-                    Text(productDetails.avgPrice != nil ? "\(grocyVM.getFormattedCurrency(amount: productDetails.avgPrice ?? 0.0)) per \(productDetails.quantityUnitStock?.name ?? "")" : "Unknown")
-                }
-                
-                Group {
-                    (Text("Average shelf life") + Text(": ")).bold()
-                    +
-                    Text(productDetails.averageShelfLifeDays  > 0 ? formatDays(daysToFormat: productDetails.averageShelfLifeDays) ?? "Unknown" : "Unknown")
-                    
-                    (Text("Spoil rate") + Text(": ")).bold()
-                    +
-                    Text("\(productDetails.spoilRatePercent.formattedAmount) %")
-                }
-                }
 
+    @AppStorage("localizationKey") var localizationKey: String = "en"
+
+    var stockElement: StockElement
+
+    var body: some View {
+        Form {
+            if let productDetails = productDetails {
+                LabeledContent(
+                    content: {
+                        Text("\(productDetails.stockAmount.formattedAmount) \(productDetails.quantityUnitStock?.getName(amount: productDetails.stockAmount) ?? "")")
+                    },
+                    label: {
+                        Label("\(Text("Stock amount")): ", systemImage: MySymbols.amount)
+                            .foregroundStyle(.primary)
+                    }
+                )
+                LabeledContent(
+                    content: {
+                        Text(grocyVM.getFormattedCurrency(amount: productDetails.stockValue ?? 0.0))
+                    },
+                    label: {
+                        Label("\(Text("Stock value")): ", systemImage: MySymbols.price)
+                            .foregroundStyle(.primary)
+                    }
+                )
+                LabeledContent(
+                    content: {
+                        Text(productDetails.location?.name ?? "")
+                    },
+                    label: {
+                        Label("\(Text("Default location")): ", systemImage: MySymbols.location)
+                            .foregroundStyle(.primary)
+                    }
+                )
+                LabeledContent(
+                    content: {
+                        VStack(alignment: .trailing) {
+                            Text(LocalizedStringKey(formatDateAsString(productDetails.lastPurchased, localizationKey: localizationKey) ?? "Never"))
+                            Text(getRelativeDateAsText(productDetails.lastPurchased, localizationKey: localizationKey) ?? "")
+                                .font(.caption)
+                                .italic()
+                        }
+                    },
+                    label: {
+                        Label("\(Text("Last purchased")): ", systemImage: MySymbols.date)
+                            .foregroundStyle(.primary)
+                    }
+                )
+                LabeledContent(
+                    content: {
+                        VStack {
+                            Text(LocalizedStringKey(formatDateAsString(productDetails.lastUsed, localizationKey: localizationKey) ?? "Never"))
+                            Text(getRelativeDateAsText(productDetails.lastUsed, localizationKey: localizationKey) ?? "")
+                                .font(.caption)
+                                .italic()
+                        }
+                    },
+                    label: {
+                        Label("\(Text("Last used")): ", systemImage: MySymbols.date)
+                            .foregroundStyle(.primary)
+                    }
+                )
+                LabeledContent(
+                    content: {
+                        Text(productDetails.lastPrice != nil ? "\(grocyVM.getFormattedCurrency(amount: productDetails.lastPrice ?? 0.0)) per \(productDetails.quantityUnitStock?.name ?? "")" : "Unknown")
+                    },
+                    label: {
+                        Label("\(Text("Last price")): ", systemImage: MySymbols.price)
+                            .foregroundStyle(.primary)
+                    }
+                )
+                LabeledContent(
+                    content: {
+                        Text(productDetails.avgPrice != nil ? "\(grocyVM.getFormattedCurrency(amount: productDetails.avgPrice ?? 0.0)) per \(productDetails.quantityUnitStock?.name ?? "")" : "Unknown")
+                    },
+                    label: {
+                        Label("\(Text("Average price")): ", systemImage: MySymbols.price)
+                            .foregroundStyle(.primary)
+                    }
+                )
+                LabeledContent(
+                    content: {
+                        Text(productDetails.averageShelfLifeDays > 0 ? formatDays(daysToFormat: productDetails.averageShelfLifeDays) ?? "Unknown" : "Unknown")
+                    },
+                    label: {
+                        Label("\(Text("Average shelf life")): ", systemImage: MySymbols.date)
+                            .foregroundStyle(.primary)
+                    }
+                )
+                LabeledContent(
+                    content: {
+                        Text("\(productDetails.spoilRatePercent.formattedAmount) %")
+                    },
+                    label: {
+                        Label("\(Text("Spoil rate")): ", systemImage: MySymbols.spoiled)
+                            .foregroundStyle(.primary)
+                    }
+                )
                 if let pictureFileName = stockElement.product?.pictureFileName {
                     PictureView(pictureFileName: pictureFileName, pictureType: .productPictures)
                         .clipShape(.rect(cornerRadius: 5.0))
